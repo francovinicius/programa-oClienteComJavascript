506162e9616283f1584fa47c75f8ddc895b434aa
Vinicius Franco Duarte <114668654+francovinicius@users.noreply.github.com>
2023-03-13 16:33:08 -0300

    Add files via upload

---
+++ codigo6-arrow-function1.js
@@ -0,0 +1,11 @@
+const multiplicacao = (n1, n2) => {
+  return n1 * n2;
+}
+console.log(multiplicacao(3, 2)); //imprimirá 6
+
+
+const divisao = (n1, n2) => {
+  return n2 / n1;
+}
+
+console.log(divisao(3,6));
